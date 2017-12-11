8ca2bc3eca3e718dc4064e8253e8ab3cf4ef78d9
Adam McKenna <adam-mckenna@outlook.com>
2017-12-11 12:39:03 +0000

    Added back button to second activity

--- app/src/main/java/com/cet325/bg72db/TicketInformationActivity.java
+++ app/src/main/java/com/cet325/bg72db/TicketInformationActivity.java
@@ -1,13 +1,37 @@
 package com.cet325.bg72db;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class TicketInformationActivity extends AppCompatActivity {
+
+    ActionBar action_bar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_information);
+        action_bar = getSupportActionBar();
+        if (action_bar != null) action_bar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }

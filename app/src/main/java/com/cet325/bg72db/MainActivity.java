e8e2b4973fe58b46d2a1697903ff10815cbcce88
Adam McKenna <adam-mckenna@outlook.com>
2017-12-11 12:16:01 +0000

    Implemented second activity and other small featuees

--- app/src/main/java/com/cet325/bg72db/MainActivity.java
+++ app/src/main/java/com/cet325/bg72db/MainActivity.java
@@ -1,9 +1,11 @@
 package com.cet325.bg72db;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         main_activity = (ConstraintLayout) findViewById(R.id.main_activity);
         registerForContextMenu(main_activity);
     }
@@ -21,5 +24,19 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_info:
+                // String name = mEditTextName.getText().toString();
+                Intent ticketInfoActivityIntent = new Intent(getApplicationContext(), TicketInformationActivity.class);
+                // myIntent.putExtra("name", name);
+                startActivity(ticketInfoActivityIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }

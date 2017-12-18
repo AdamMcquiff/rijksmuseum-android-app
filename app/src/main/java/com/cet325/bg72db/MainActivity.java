12cab22b63a852941f4ab5b5924090b512b8f88c
Adam McKenna <adam-mckenna@outlook.com>
2017-12-18 15:27:52 +0000

    Added event listeners, began implementation of currency conversion

--- app/src/main/java/com/cet325/bg72db/MainActivity.java
+++ app/src/main/java/com/cet325/bg72db/MainActivity.java
@@ -5,21 +5,31 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO: Add CTA button event listener
-
     ScrollView main_activity = null;
+    Button call_to_action_btn = null;
+
+    private View.OnClickListener call_to_action_event_listener = new View.OnClickListener() {
+        public void onClick(View view) {
+            // TODO; add intent / event logic
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        main_activity = (ScrollView) findViewById(R.id.main_activity);
+        main_activity = findViewById(R.id.main_activity);
         registerForContextMenu(main_activity);
+
+        call_to_action_btn = findViewById(R.id.discover_art_btn);
+        call_to_action_btn.setOnClickListener(call_to_action_event_listener);
     }
 
     @Override
@@ -33,9 +43,9 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.action_info:
                 // String name = mEditTextName.getText().toString();
-                Intent ticketInfoActivityIntent = new Intent(getApplicationContext(), TicketInformationActivity.class);
+                Intent ticket_info_activity_intent = new Intent(getApplicationContext(), TicketInformationActivity.class);
                 // myIntent.putExtra("name", name);
-                startActivity(ticketInfoActivityIntent);
+                startActivity(ticket_info_activity_intent);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
--- app/src/main/java/com/cet325/bg72db/TicketInformationActivity.java
+++ app/src/main/java/com/cet325/bg72db/TicketInformationActivity.java
@@ -1,21 +1,45 @@
 package com.cet325.bg72db;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class TicketInformationActivity extends AppCompatActivity {
 
     ActionBar action_bar = null;
+    TextView prices_textview = null;
+    FloatingActionButton currency_exchange_btn = null;
+
+    static double TICKET_PRICE_EURO = 10.00;
+    // TODO: get user currency OR default
+    String selected_currency = "EUR";
+
+    private View.OnClickListener currency_exchange_event_listener = new View.OnClickListener() {
+        public void onClick(View view) {
+            buildCurrencyDialog();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_information);
+
         action_bar = getSupportActionBar();
         if (action_bar != null) action_bar.setDisplayHomeAsUpEnabled(true);
+
+        prices_textview = findViewById(R.id.prices_textview);
+        updatePricesTextView(selected_currency);
+
+        currency_exchange_btn = findViewById(R.id.currency_action_button);
+        currency_exchange_btn.setOnClickListener(currency_exchange_event_listener);
     }
 
     @Override
@@ -33,5 +57,39 @@ public class TicketInformationActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void buildCurrencyDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.currency_dialog_title);
+        builder.setItems(R.array.currencies, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int selected) {
+                String[] currencies = getResources().getStringArray(R.array.currencies);
+                String[] currency_codes = getResources().getStringArray(R.array.currency_codes);
+                for (int i = 0; i < currencies.length; i++) {
+                    if (selected == i) {
+                        selected_currency = currency_codes[i];
+                        updatePricesTextView(selected_currency);
+                        System.out.println("Current index is: " + i);
+                    }
+                }
+            }
+        });
+        AlertDialog ad = builder.create();
+        ad.show();
+    }
+
+    private void updatePricesTextView(String currency) {
+        String string = "Adult (18+): ";
+        switch (currency) {
+            case "EUR":
+                string += "€10,00 \nStudent: €7,00";
+                break;
+            case "GDP":
+                string += "£10.00 \nStudent: £7.00";
+                break;
+        }
+        string += "\nChildren (under 18): FREE";
+        prices_textview.setText(string);
     }
 }
